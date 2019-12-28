138c2876af7da51fc3aa59ff9b96c239bbd0a0a5
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-28 09:44:25 +0430

    data model

--- src/main/java/af/asr/identity/data/model/Tenant.java
+++ src/main/java/af/asr/identity/data/model/Tenant.java
@@ -1,12 +1,9 @@
 package af.asr.identity.data.model;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.Collection;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,29 +12,20 @@ import java.util.Collection;
 @ToString
 @EqualsAndHashCode
 @Entity
-@Table(name = "roles")
-public class Role {
+@Table(name = "tenants")
+public class Tenant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    @NotNull(message = "Role name can't be null")
+    @NotNull(message = "Tenant name can't be null")
     @Column(nullable = false, unique = true)
     private String name;
     @Column
     private String description;
-
     @Column(name = "active", length = 1, nullable = false)
     private boolean active;
-
     @Column(name = "is_core", length = 1, nullable = false)
     private boolean core;
 
-
-    @ManyToMany(fetch=FetchType.LAZY)
-    @JoinTable(name = "role_privilges",
-            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "privilege_id", referencedColumnName = "id"))
-    @JsonIgnore
-    private Collection<Privilege> privileges;
 }
