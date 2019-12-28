7d1f558777892a8e4daf2ad1751b801b6833e8d1
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-28 09:40:20 +0430

    data model

--- src/main/java/af/asr/identity/data/model/Privilege.java
+++ src/main/java/af/asr/identity/data/model/Privilege.java
@@ -1,4 +1,27 @@
 package af.asr.identity.data.model;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString(exclude="roles")
+@EqualsAndHashCode
+@Entity
+@Table(name = "privileges")
 public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull(message = "Privilege name can't be null")
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column
+    private String description;
 }
