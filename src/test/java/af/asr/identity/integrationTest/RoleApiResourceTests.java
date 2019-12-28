211201a4d3dd08018c9d474251a3b79325e0b4d0
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-28 13:57:45 +0430

    role integration tests

---
+++ src/test/java/af/asr/identity/integrationTest/RoleApiResourceTests.java
@@ -0,0 +1,38 @@
+package af.asr.identity.integrationTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class RoleApiResourceTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    TestRestTemplate restTemplate = new TestRestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+
+
+
+    @WithMockUser("ADMIN")
+    @Test
+    public void testCreateRetrieveWithMockMVC() throws Exception {
+        this.mockMvc.perform(get("/api/privileges")).andExpect(status().is2xxSuccessful());
+        this.mockMvc.perform(get("//api/privileges")).andDo(print()).andExpect(status().isOk());
+    }
+}
