485c9b016494b929e0c599c1b488ca7e8f625c44
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-28 11:04:11 +0430

    auditing and database revisioning

---
+++ src/main/java/af/asr/identity/data/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package af.asr.identity.data.repository;
+
+import af.asr.identity.data.model.Tenant;
+import af.asr.identity.data.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, RevisionRepository<User, Long, Long> {
+}
+
