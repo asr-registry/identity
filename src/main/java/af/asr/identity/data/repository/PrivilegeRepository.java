c2a2a05e842c280640c403c288d53ef45daf99a5
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-28 11:06:13 +0430

    auditing and database revisioning

--- src/main/java/af/asr/identity/data/repository/PrivilegeRepository.java
+++ src/main/java/af/asr/identity/data/repository/PrivilegeRepository.java
@@ -1,10 +1,11 @@
 package af.asr.identity.data.repository;
 
-import af.asr.identity.data.model.Group;
+import af.asr.identity.data.model.Privilege;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroupRepository extends JpaRepository<Group, Long>, RevisionRepository<Group, Long, Long> {
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long>, RevisionRepository<Privilege, Long, Long> {
 }
+
